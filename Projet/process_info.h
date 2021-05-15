c4efc7d6a1cddeb955e0a8e0ee61524413ba44a6
Benjamin <b14.kopf96@gmail.com>
2021-05-15 12:09:22 +0200

    bug

--- Projet/process_info.h
+++ Projet/process_info.h
@@ -4,7 +4,7 @@
 uint16_t get_line_width(void);
 uint16_t get_line_position(void);
 void process_image_start(void);
-uint16_t get_proxii(void);
+uint16_t *get_proxi(void);
 uint8_t get_compteur_liigne(void);
 int8_t get_inclined(void);
 
